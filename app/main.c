79a0e6a0ce068ceeea2f568d36abd6516199e013
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2025-09-28 19:51:33 +0530

    My bad, my bad, had to recomment a comment

--- app/main.c
+++ app/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
 
-	Uncomment this block to pass the first stage
+	// Uncomment this block to pass the first stage
 	
 	char *command = argv[3];
 	int child_pid = fork();
